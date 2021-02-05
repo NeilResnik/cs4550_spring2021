840ea1ba05a2bb59323a794b86cebb5630e9e0d8
Neil Resnik <neilr22@comcast.net>
2021-02-05 13:45:36 -0500

    Finished up javascript and added CSS styling

--- hw03/four_digits/src/App.js
+++ hw03/four_digits/src/App.js
@@ -1,8 +1,21 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
 
-function GuessInput({guess}) {
+var secret = generateSecret();
+
+function generateSecret() {
+    let options = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let secret = "";
+    while(secret.length < 4) {
+        let idx = Math.floor(Math.random() * options.length);
+        secret += options[idx];
+        options.splice(idx, 1);
+    }
+    return secret;
+}
+
+function GuessInput({guess, reset, enabled}) {
     const [text, setText] = useState("");
 
     function updateText(event) {
@@ -17,38 +30,52 @@ function GuessInput({guess}) {
     }
 
     function onKeypress(event) {
-        if(event.key === "Enter"){
+        if(event.key === "Enter" && text.length === 4){
             guess(text);
         }
     }
 
     return (
-        <p>
-            <input type="text" 
-                onChange={updateText}
-                onKeyPress={onKeypress}
-                value={text} />
-                <button onClick={() => { guess(text); }}> Guess </button>
-        </p>
+        <div className="field has-addons has-addons-centered">
+            <div className="control">
+                <input className="input"
+                    type="text" 
+                    placeholder="Guess"
+                    onChange={updateText}
+                    onKeyPress={onKeypress}
+                    disabled={!enabled}
+                    value={text}/>
+            </div>
+            <div className="control">
+                <button className="button is-primary"
+                        disabled={!enabled || text.length !== 4}
+                        onClick={() => { guess(text); }}>
+                    Guess 
+                </button>
+            </div>
+            <div className="control">
+                <button className="button is-danger"
+                        onClick={reset}>
+                    Reset
+                </button>
+            </div>
+        </div>
     );
-
 }
 
-function generateSecret() {
-    return "1234";
-}
 
 function App() {
     const [gameState, setGameState] = useState({
-        secret: generateSecret(),
+        secret: secret,
         guesses: [],
         results: [],
-        over: false,
+        correct: false,
     });
 
     function guess(text){
-        console.log(gameState.secret);
-        console.log(text);
+        if(text.length < 4) {
+            return;
+        }
 
         let key = gameState.secret;
         let correct = 0;
@@ -71,54 +98,56 @@ function App() {
             secret: gameState.secret,
             guesses: gameState.guesses,
             results: gameState.results,
-            over: correct === 4,
+            correct: correct === 4,
         })
-    }
-
-    function getDisplay(guesses, results) {
-        let display = "";
-        for(let i = 0; i < guesses.length; i++) {
-            display += guesses[i];
-            display += ": ";
-            display += results[i];
-            display += "\n";
-        }
-        return display;
     }
 
     function reset() {
+        secret = generateSecret();
         setGameState({
-            secret: generateSecret(),
+            secret: secret,
             guesses: [],
             results: [],
-            over: false,
+            correct: false,
         })
     }
-    let body = null;
-    let resetButton = (
-       <button onClick={reset}>Reset</button>
-    );
-
-    if(!gameState.over) {
-       body = (
-           <div>
-               <GuessInput guess={guess}/>
-               {resetButton}
-               <h1> Guesses </h1>
-               <p> {getDisplay(gameState.guesses, gameState.results)} </p>
-           </div>
-      );
+    let notificationClassName = "notification";
+    if(gameState.correct) {
+        notificationClassName += " is-primary";
+    } else if(gameState.guesses.length >= 8){
+        notificationClassName += " is-danger";
     } else {
-        body = (
-            <div>
-                <h1>Game Over</h1>
-               {resetButton}
-            </div>
-        );
+        notificationClassName += " is-hidden";
     }
+
     return (
         <div className="App">
-            {body}
+           <section className="section has-background-grey-light">
+               <div className="container has-text-centered">
+                    <div className={notificationClassName}>
+                        <p>{gameState.correct ? "You Won!" : "You Lost!"}</p>
+                        <p> Correct Answer: {gameState.secret}</p>
+                    </div>
+                   <GuessInput guess={guess}
+                       reset={reset}
+                       enabled={!gameState.correct && gameState.guesses.length < 8}/>
+                    <p className="is-size-3">Lives: {8 - gameState.guesses.length}</p>
+               </div>
+           </section>
+           <section className="section">
+               <div className="container has-text-centered">
+                   <h1 className="title is-size-2">Guesses:</h1>
+                       <ul>
+                           {gameState.guesses.map(function(e, i){
+                               return (
+                                   <li key={e}>
+                                       <p>{e}: {gameState.results[i]}</p>
+                                   </li>
+                               );
+                           })}
+                       </ul>
+               </div>
+           </section>
         </div>
     );
 }
